833f08f553e05b1dee579c798cdf403a17cbd15c
stein <stein@salver.no>
2018-12-12 12:55:20 +0100

    Initial Commit

---
+++ RayTracerB5/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  RayTracerB5
+//
+//  Created by Stein Alver on 12/12/2018.
+//  Copyright © 2018 Stein Alver. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
